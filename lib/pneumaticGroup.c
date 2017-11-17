277c3cde1c99b542d0d92cad400240634719d018
trsigg <trsigg@gmail.com>
2017-11-16 22:34:10 -0700

    pneumaticGroup fixes

--- lib/pneumaticGroup.c
+++ lib/pneumaticGroup.c
@@ -4,16 +4,16 @@ enum pneumaticControlType { P_NONE, TOGGLE, TWO_BTN };
 
 typedef struct {
   tSensors solenoids[10];
-  int nulSolenoids;
+  int numSolenoids;
   //button control
   TVexJoysticks openOrToggleBtn, closeBtn;
   pneumaticControlType controlScheme;
-  bool isOpen
+  bool isOpen;
 } pneumaticGroup;
 
 void initializePneumaticGroup(pneumaticGroup *group, int numSolenoids, tSensors sol1, tSensors sol2=dgtl1, tSensors sol3=dgtl1, tSensors sol4=dgtl1, tSensors sol5=dgtl1, tSensors sol6=dgtl1, tSensors sol7=dgtl1, tSensors sol8=dgtl1, tSensors sol9=dgtl1, tSensors sol10=dgtl1) {
   tSensors solenoids[10] = { sol1, sol2, sol3, sol4, sol5, sol6, sol7, sol8, sol9, sol10 };
-  for (int i=0; i<numMotors; i++)
+  for (int i=0; i<numSolenoids; i++)
     group->solenoids[i] = solenoids[i];
 
   group->numSolenoids = numSolenoids;
@@ -33,7 +33,7 @@ void configureToggleInput(pneumaticGroup *group, TVexJoysticks toggleBtn) {
 
 void setState(pneumaticGroup *group, bool open) {
   for (int i=0; i<group->numSolenoids; i++)
-    group->solenoids[i] = open; //true casts to 1, false to 0
+    SensorValue[ group->solenoids[i] ] = open; //true casts to 1, false to 0
 
   group->isOpen = open;
 }
